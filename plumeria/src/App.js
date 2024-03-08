1c3227ea9f4ed2319b82c2dfbe8d2a0ee1b271af
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-03-07 21:23:12 -0800

    Adding tab links

--- plumeria/src/App.js
+++ plumeria/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+
+        {/* <a href="#">Locations</a> |
+        <a href="#">Menu</a> |
+        <a class="navbar-brand" href="/">
+          <div class="logo-image">
+
+            <img className="img-fluid"src={require('./Plumeria Logo.png')} alt="Plumeria Logo"/>
+          </div>
+        </a> 
+        <a href="#">Order Online</a> |
+        <a href="#">About Us</a>  */}
+
+
+
+
+        {/* <a class="navbar-brand" href="/">
+          <div class="logo-image">
+
+            <img className="img-fluid"src={require('./Plumeria Logo.png')} alt="Plumeria Logo"/>
+          </div>
+        </a>  */}
+
+      </nav>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ plumeria/src/Navbar.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        
+        <nav className="navbar">
+            <div className="tabLinks">
+                <Link className= "link" to="/Locations">Locations</Link>
+                <Link className= "link" to="/Menu">Menu</Link>
+                <Link className= "link" to="/Sale">Sale</Link>
+                <Link className= "link" to="/Order">Order Online</Link>
+                <Link className= "link" to="/About">About Us</Link>
+
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
