4d50f27baf7e3742491f47d6635a319a59da63c5
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-03-23 13:24:47 -0700

    Added logo to navbar

--- plumeria/src/Navbar.js
+++ plumeria/src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <div className="tabLinks">
                 <Link className= "link" to="/Locations">Locations</Link>
                 <Link className= "link" to="/Menu">Menu</Link>
-                <Link className= "link" to="/">Home</Link>
+                <Link className= "link" to="/"><img className="img-fluid"src={require('./Plumeria Logo.png')} alt="Plumeria Logo"/> </Link>
                 <Link className= "link" to="/Order">Order Online</Link>
                 <Link className= "link" to="/About">About Us</Link>
 
