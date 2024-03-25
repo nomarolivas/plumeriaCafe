a5b887a2c9e65a8d2cf49e1406f2c2232de3a8c0
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-03-24 20:31:35 -0700

    Added more css to navbar

--- plumeria/src/Navbar.js
+++ plumeria/src/Navbar.js
@@ -2,18 +2,31 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
+        <div className='navbarClass'>
+            <nav className="navbar">
+                <div className="tabLinks">
+                    <Link className= "link" to="/Locations">Locations</Link>
+                    <Link className= "link" to="/Menu">Menu</Link>
+                    <Link className= "logoLink" to="/"><img className="img-fluid"src={require('./Plumeria Logo.png')} alt="Plumeria Logo"/> </Link>
+                    <Link className= "link" to="/Order">Order Online</Link>
+                    <Link className= "link" to="/About">About Us</Link>
+
+                </div>
+
+            </nav>
+
+
+
+
+        </div>
         
-        <nav className="navbar">
-            <div className="tabLinks">
-                <Link className= "link" to="/Locations">Locations</Link>
-                <Link className= "link" to="/Menu">Menu</Link>
-                <Link className= "link" to="/"><img className="img-fluid"src={require('./Plumeria Logo.png')} alt="Plumeria Logo"/> </Link>
-                <Link className= "link" to="/Order">Order Online</Link>
-                <Link className= "link" to="/About">About Us</Link>
 
-            </div>
 
-        </nav>
+
+
+
+
+
     );
 }
 
