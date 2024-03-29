22b7590844eabcf0ad48c8dd6f89eb746b92debd
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-03-28 20:57:24 -0700

    Added dropdown for Menu

--- plumeria/src/Navbar.js
+++ plumeria/src/Navbar.js
@@ -8,19 +8,31 @@ const Navbar = () => {
                     <Link className= "link" to="/Locations">Locations</Link>
                     {/* <Link className= "link" to="/Menu">Menu</Link> */}
 
-
                     <div class="dropdown">
-                        <button class="dropbtn">Menu
+                        <button class="menuBtn">Menu
                         <i class="fa fa-caret-down"></i>
                         </button>
-                        <div class="dropdown-content">
-                        <button>Menu 1</button>
-                        <button>Menu 2</button>
-                        <button>Menu 3</button>
+                        <div className='dropDownMenu'>
+                            <button>Menu 1</button>
+                            <button>Menu 2</button>
+                            <button>Menu 3</button>
                         </div>
                     </div>
+
                     <Link className= "logoLink" to="/"><img className="img-fluid"src={require('./Plumeria Logo.png')} alt="Plumeria Logo"/> </Link>
                     <Link className= "link" to="/Order">Order Online</Link>
+
+                    {/* <div class="dropdown">
+                        <button class="menuBtn">Order Online
+                        <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div className='dropDownMenu'>
+                            <button>Menu 1</button>
+                            <button>Menu 2</button>
+                            <button>Menu 3</button>
+                        </div>
+                    </div> */}
+                    
                     <Link className= "link" to="/About">About Us</Link>
 
                 </div>
