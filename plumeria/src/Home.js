a0bc2ff3150ecfb0306c03b77fa4a7cf7b343305
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2024-04-09 18:29:39 -0700

    Added slideshow and images

--- plumeria/src/Home.js
+++ plumeria/src/Home.js
@@ -1,6 +1,43 @@
+import React, { useState } from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+
+const sliderImages = [
+    { url: "/Users/nomarolivas/Documents/GitHub/plumeriaCafe/plumeria/src/Images/Stacks Pic 1.jpeg" },
+    { url: "/Users/nomarolivas/Documents/GitHub/plumeriaCafe/plumeria/src/Images/Stacks Pic 1.jpeg" },
+    { url: "images/3.jpg" },
+    { url: "images/4.jpg" },
+    { url: "images/5.jpg" },
+    { url: "images/6.jpg" },
+    { url: "images/7.jpg" },
+  ];
+
 const Home = () => {
+    const [imageNum, setImageNum] = useState(1);
     return (
-        <h1>Home Page</h1>
+ 
+        <div className="image-slider">
+            <SimpleImageSlider
+                style={{ margin: '0 auto', padding: '0', width: '100% auto'}}
+                    // width= {1070} 
+                    width= "100%"
+                    height={600}
+
+                    navMargin={0}
+                    images={sliderImages}
+                    showBullets={true}
+                
+
+                    showNavs={true}
+                    autoPlay={true} 
+                    onStartSlide = {(index, length) => {
+                        setImageNum(index);
+                    }}
+                    autoPlayDelay = {8}
+                            
+                />
+
+            </div>  
+        
 
     );
 }
